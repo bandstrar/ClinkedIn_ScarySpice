ecc3dafcde6ab38d3c4d674a77826284a6ce6d9f
Robert Bandstra <rob.bandstra@gmail.com>
2021-02-22 19:53:01 -0600

    added cast of characters

--- ClinkedIn/DataAccess/ClinkerRepository.cs
+++ ClinkedIn/DataAccess/ClinkerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClinkedIn.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace ClinkedIn.DataAccess
 {
     public class ClinkerRepository
     {
+        static List<Clinker> _loaves = new List<Clinker>
+        {
+            new Clinker {SerialNumber = 1, Name = "Joe Exotic", Interests = new List<string> {"Exotic animals", "Cowboy hats" }, Services = new List<string> {"Relationship advice", "Hiring hitmen" } },
+            new Clinker {SerialNumber = 2, Name = "Al Capone", Interests = new List<string> {"Tax evasion", "Al Pacino movies" }, Services = new List<string> {"Hiring bodyguards", "Smuggling goods" } },
+            new Clinker {SerialNumber = 3, Name = "John Dillinger", Interests = new List<string> {"Banks", "Broads" }, Services = new List<string> {"Making escape plans", "Stealing cafeteria food" } },
+            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", Interests = new List<string> {"DIY Vehicles", "Rock bowling" }, Services = new List<string> {"Shank sharpening", "Selling exotic animals" } }
+        };
     }
 }
