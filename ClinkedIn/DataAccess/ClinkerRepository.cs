bfb27d978a89cc20d9b1f18a50809e53896663df
Robert Bandstra <rob.bandstra@gmail.com>
2021-02-25 19:05:36 -0600

    added automatic re-friending

--- ClinkedIn/DataAccess/ClinkerRepository.cs
+++ ClinkedIn/DataAccess/ClinkerRepository.cs
@@ -11,7 +11,7 @@ namespace ClinkedIn.DataAccess
             new Clinker {SerialNumber = 1, Name = "Joe Exotic", Interests = new List<string> {"Exotic animals", "Cowboy hats" }, Services = new List<string> {"Relationship advice", "Hiring hitmen" } },
             new Clinker {SerialNumber = 2, Name = "Al Capone", Interests = new List<string> {"Tax evasion", "Al Pacino movies" }, Services = new List<string> {"Hiring bodyguards", "Smuggling goods" } },
             new Clinker {SerialNumber = 3, Name = "John Dillinger", Interests = new List<string> {"Banks", "Broads" }, Services = new List<string> {"Making escape plans", "Stealing cafeteria food" } },
-            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", Interests = new List<string> {"DIY Vehicles", "Rock bowling" }, Services = new List<string> {"Shank sharpening", "Selling exotic animals" } }
+            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", Interests = new List<string> {"DIY Vehicles", "Rock bowling" }, Services = new List<string> {"Shank sharpening", "Selling exotic animals" } },
         };
 
         public List<Clinker> GetAll()
@@ -71,6 +71,10 @@ namespace ClinkedIn.DataAccess
             if (!clinker.Friends.Contains(friend))
             {
                 clinker.Friends.Add(friend);
+            }
+            if (!friend.Friends.Contains(clinker))
+            {
+                friend.Friends.Add(clinker);
             }
             
         }
