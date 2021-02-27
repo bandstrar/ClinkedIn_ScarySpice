c0d637ab46cb76a65c4ec972f4e96903154a1915
Jordan Smith <jordansmith989@gmail.com>
2021-02-27 10:59:10 -0600

    mergin

--- ClinkedIn/DataAccess/ClinkerRepository.cs
+++ ClinkedIn/DataAccess/ClinkerRepository.cs
@@ -8,15 +8,15 @@ namespace ClinkedIn.DataAccess
     {
         static List<Clinker> _clinkers = new List<Clinker>
         {
-            new Clinker {SerialNumber = 1, Name = "Joe Exotic", Interests = new List<string> {"Exotic animals", "Cowboy hats" }, Services = new List<string> {"Relationship advice", "Hiring hitmen" } },
-            new Clinker {SerialNumber = 2, Name = "Al Capone", Interests = new List<string> {"Tax evasion", "Al Pacino movies" }, Services = new List<string> {"Hiring bodyguards", "Smuggling goods" } },
-            new Clinker {SerialNumber = 3, Name = "John Dillinger", Interests = new List<string> {"Banks", "Broads" }, Services = new List<string> {"Making escape plans", "Stealing cafeteria food" } },
-            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", Interests = new List<string> {"DIY Vehicles", "Rock bowling" }, Services = new List<string> {"Shank sharpening", "Selling exotic animals" } },
-            new Clinker {SerialNumber = 5, Name = "Clyde Barrow", Interests = new List<string> {"Jazz Music", "Robbery" }, Services = new List<string> {"Hideouts", "Apple Pie" } },
-            new Clinker {SerialNumber = 6, Name = "Frank Abagnale", Interests = new List<string> {"Traveling", "Fine food" }, Services = new List<string> {"Disgusies", "Counterfeiting" } },
-            new Clinker {SerialNumber = 7, Name = "Charlie Manson", Interests = new List<string> {"Rock N Roll", "Knives" }, Services = new List<string> {"Organizing Retreats", "Face tattoos" } },
-            new Clinker {SerialNumber = 8, Name = "George Bluth", Interests = new List<string> {"The Banana Stand", "Cornballers" }, Services = new List<string> {"Motivational Tapes", "Smuggling goods" } },
-            new Clinker {SerialNumber = 9, Name = "Hannibal Lecter", Interests = new List<string> {"Fine Cuisine", "Face Masks" }, Services = new List<string> {"Psychological Advice", "Cooking" } }
+            new Clinker {SerialNumber = 1, Name = "Joe Exotic", Interests = new List<string> {"Exotic animals", "Cowboy hats" }, Services = new List<string> {"Relationship advice", "Hiring hitmen" }, DaysRemaining = 1 },
+            new Clinker {SerialNumber = 2, Name = "Al Capone", Interests = new List<string> {"Tax evasion", "Al Pacino movies" }, Services = new List<string> {"Hiring bodyguards", "Smuggling goods" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 3, Name = "John Dillinger", Interests = new List<string> {"Banks", "Broads" }, Services = new List<string> {"Making escape plans", "Stealing cafeteria food" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", Interests = new List<string> {"DIY Vehicles", "Rock bowling" }, Services = new List<string> {"Shank sharpening", "Selling exotic animals" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 5, Name = "Clyde Barrow", Interests = new List<string> {"Jazz Music", "Robbery" }, Services = new List<string> {"Hideouts", "Apple Pie" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 6, Name = "Frank Abagnale", Interests = new List<string> {"Traveling", "Fine food" }, Services = new List<string> {"Disgusies", "Counterfeiting" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 7, Name = "Charlie Manson", Interests = new List<string> {"Rock N Roll", "Knives" }, Services = new List<string> {"Organizing Retreats", "Face tattoos" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 8, Name = "George Bluth", Interests = new List<string> {"The Banana Stand", "Cornballers" }, Services = new List<string> {"Motivational Tapes", "Smuggling goods" }, DaysRemaining = 1  },
+            new Clinker {SerialNumber = 9, Name = "Hannibal Lecter", Interests = new List<string> {"Fine Cuisine", "Face Masks" }, Services = new List<string> {"Psychological Advice", "Cooking" }, DaysRemaining = 1  }
         };
 
         public List<Clinker> GetAll()
