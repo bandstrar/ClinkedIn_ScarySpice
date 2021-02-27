2e382cd1a433e1258c0db6626556fc405afafff2
Robert Bandstra <rob.bandstra@gmail.com>
2021-02-27 12:42:18 -0600

    updated endpoint comments and instantiated services and interests

--- ClinkedIn/Models/Clinker.cs
+++ ClinkedIn/Models/Clinker.cs
@@ -9,8 +9,8 @@ namespace ClinkedIn.Models
     {
         public int SerialNumber { get; set; }
         public string Name { get; set; }
-        public List<string> Interests { get; set; }
-        public List<string> Services { get; set; }
+        public List<string> Interests { get; set; } = new List<string>();
+        public List<string> Services { get; set; } = new List<string>();
         public List<Clinker> Friends { get; set; } = new List<Clinker>();
         public List<Clinker> Enemies { get; set; } = new List<Clinker>();
         public int DaysRemaining { get; set; }
