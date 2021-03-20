aa5e1dbf2a1f323c1e174fc2f4dbbec612ba6e19
Robert Bandstra <rob.bandstra@gmail.com>
2021-03-19 19:50:08 -0500

    updated get all clinkers method

--- ClinkedIn/DataAccess/ClinkerRepository.cs
+++ ClinkedIn/DataAccess/ClinkerRepository.cs
@@ -2,27 +2,34 @@ using ClinkedIn.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace ClinkedIn.DataAccess
 {
     public class ClinkerRepository
     {
+        const string ConnectionString = "Server=localhost;Database=ClinkedIn;Trusted_Connection=True;";
         static List<Clinker> _clinkers = new List<Clinker>
         {
-            new Clinker {SerialNumber = 1, Name = "Joe Exotic", Interests = new List<string> {"Exotic animals", "Cowboy hats" }, Services = new List<string> {"Relationship advice", "Hiring hitmen" }, DaysRemaining = 1 },
-            new Clinker {SerialNumber = 2, Name = "Al Capone", Interests = new List<string> {"Tax evasion", "Al Pacino movies" }, Services = new List<string> {"Hiring bodyguards", "Smuggling goods" }, DaysRemaining = 12  },
-            new Clinker {SerialNumber = 3, Name = "John Dillinger", Interests = new List<string> {"Banks", "Broads" }, Services = new List<string> {"Making escape plans", "Stealing cafeteria food" }, DaysRemaining = 1000  },
-            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", Interests = new List<string> {"DIY Vehicles", "Rock bowling" }, Services = new List<string> {"Shank sharpening", "Selling exotic animals" }, DaysRemaining = 200  },
-            new Clinker {SerialNumber = 5, Name = "Clyde Barrow", Interests = new List<string> {"Jazz Music", "Robbery" }, Services = new List<string> {"Hideouts", "Apple Pie" }, DaysRemaining = 420  },
-            new Clinker {SerialNumber = 6, Name = "Frank Abagnale", Interests = new List<string> {"Traveling", "Fine food" }, Services = new List<string> {"Disgusies", "Counterfeiting" }, DaysRemaining = 12983  },
-            new Clinker {SerialNumber = 7, Name = "Charlie Manson", Interests = new List<string> {"Rock N Roll", "Knives" }, Services = new List<string> {"Organizing Retreats", "Face tattoos" }, DaysRemaining = 329  },
-            new Clinker {SerialNumber = 8, Name = "George Bluth", Interests = new List<string> {"The Banana Stand", "Cornballers" }, Services = new List<string> {"Motivational Tapes", "Smuggling goods" }, DaysRemaining = 2  },
-            new Clinker {SerialNumber = 9, Name = "Hannibal Lecter", Interests = new List<string> {"Fine Cuisine", "Face Masks" }, Services = new List<string> {"Psychological Advice", "Cooking" }, DaysRemaining = 1  }
+            new Clinker {SerialNumber = 1, Name = "Joe Exotic", DaysRemaining = 1 },
+            new Clinker {SerialNumber = 2, Name = "Al Capone", DaysRemaining = 12  },
+            new Clinker {SerialNumber = 3, Name = "John Dillinger", DaysRemaining = 1000  },
+            new Clinker {SerialNumber = 4, Name = "Fred Flintstone", DaysRemaining = 200  },
+            new Clinker {SerialNumber = 5, Name = "Clyde Barrow", DaysRemaining = 420  },
+            new Clinker {SerialNumber = 6, Name = "Frank Abagnale", DaysRemaining = 12983  },
+            new Clinker {SerialNumber = 7, Name = "Charlie Manson", DaysRemaining = 329  },
+            new Clinker {SerialNumber = 8, Name = "George Bluth", DaysRemaining = 2  },
+            new Clinker {SerialNumber = 9, Name = "Hannibal Lecter", DaysRemaining = 1  }
         };
 
         public List<Clinker> GetAll()
         {
-            return _clinkers;
+            using var db = new SqlConnection(ConnectionString);
+            var sql = @"Select *
+                        from Clinkers";
+
+            return db.Query<Clinker>(sql).ToList();
         }
 
         public Clinker Get(int serialNumber)
